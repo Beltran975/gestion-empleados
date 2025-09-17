679e8de9f6b4e02e2fab1a20882ac299ee62b107
Diego Beltran Hernandez <diegobeltran973@gmail.com>
2025-09-16 23:02:35 -0600

    Se agrego la funcion para editar empleados en el controlador de empleado

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware(['auth.session'])->group(function () {
     Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
     Route::get('/empleados/create', [EmpleadoController::class, 'create'])->name('empleados.create');
     Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados.store');
+    Route::get('/empleados/{empleado}/edit', [EmpleadoController::class, 'edit'])->name('empleados.edit');
+    Route::put('/empleados/{empleado}', [EmpleadoController::class, 'update'])->name('empleados.update');
 });
